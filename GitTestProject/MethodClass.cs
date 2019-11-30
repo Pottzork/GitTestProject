acaa67db8a436ffb2ae7dd1f6fa6bbe2717b5d3c
André Pontes <33063611+Pottzork@users.noreply.github.com>
2019-11-30 22:50:31 +0100

    Add project files.

---
+++ GitTestProject/MethodClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestProject
+{
+    public class MethodClass
+    {
+
+        public void CheckAge(int age)
+        {
+            string tenYear = "Wow you are over ten year old";
+            string underten = "Babyface";
+
+            if (age > 10)
+            {
+                Console.WriteLine(tenYear);
+            }
+            else
+            {
+                Console.WriteLine(underten); ;
+            }
+        }
+
+
+        public void TellAge()
+        {
+            
+
+            Console.WriteLine("Enter age: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            CheckAge(userInput);
+
+
+        }
+
+    }
+}
---
+++ GitTestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MethodClass test = new MethodClass();
+
+            test.TellAge();
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
